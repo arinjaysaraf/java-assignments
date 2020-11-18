3d3776bba63078d6bd099865faeb68ae60deb28c
arinjaysaraf <56652667+arinjaysaraf@users.noreply.github.com>
2020-11-18 14:52:30 +0530

    Update Swap.java

--- Assignment 1/Swap.java
+++ Assignment 1/Swap.java
@@ -3,9 +3,9 @@ public class Swap {
         int a = 2, b = 3;
         // Output : 2 3
         System.out.println("Before Swapping : " + a + " " + " " + b);
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
         // Output : 3 2
         System.out.println("After Swapping : " + a + " " + " " + b);
     }
